470ba52384e6a1e2bb49a12f89106d654048be1c
Quinn <qkfreas@gmail.com>
2017-06-14 15:43:20 -0400

    Did... something

--- app/src/main/java/com/example/quinn/myapplication/MyPage.java
+++ app/src/main/java/com/example/quinn/myapplication/MyPage.java
@@ -8,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import java.io.InputStream;
 import java.net.URL;
 
+import static com.example.quinn.myapplication.R.layout.my_page;
+
 /**
  * Created by quinn on 6/5/17.
  */
@@ -16,18 +18,23 @@ public class MyPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.my_page);
+        setContentView(my_page);
 
         Intent intent = getIntent();
     }
 
     public static Drawable LoadImageFromWebOperations(String url) {
+
+//        http://api.hoopstacam.sashimiblade.com/parse
+//        https://github.com/parse-community/Parse-SDK-Android
         try {
-            InputStream is = (InputStream) new URL(url).getContent();
+            InputStream is = (InputStream) new URL("http://api.hoopstacam.sashimiblade.com/parse").getContent();
             Drawable d = Drawable.createFromStream(is, "src name");
             return d;
         } catch (Exception e) {
             return null;
+        }
+        finally {
         }
     }
 }
